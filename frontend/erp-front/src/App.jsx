c066b71b5b6fe737e41929ab8ee04bd0d2cc084d
Lucas David <lucas_david04@outlook.com>
2026-07-16 21:47:40 -0300

    inserct page of products

--- frontend/erp-front/src/App.jsx
+++ frontend/erp-front/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './pages/login'
 import Home from './pages/home'
+import Products from './pages/products'
 
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/materials' element={<Home/>}/>
+        <Route path='/sheets' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
 
